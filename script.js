6282e32342d77612e04a306712013a21f0915118
Kouassi Kevin Kouadio <kevkouadio>
2020-08-03 12:02:22 -0400

    set display current weather

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-// Initial array of movies
+// Initial array for cities buttons
 var cities = [];
 
-// Generic function for capturing the movie name from the data-attribute
+$("#searchcity").click(function displayWeather(){
+    var city = $("#city-input").val();
 
+    const APIKey = "d9680370698e25d5baff0233989f8bbc";
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        
+      $(".cityDate").text(response.name);
+      $('.temp').text('Temp: '+ response.main.temp + ' F');
+      $('.wind').text("Wind: "+ response.wind.speed +" MPH");
+      $('.humidity').text("Humidity: " + response.main.humidity +'%');
+        console.log(response);
+        
+      });  
+}); 
+
+// creating cities buttons 
  function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise we will have repeat buttons)
     $("#cityBtn").empty();
 
-    // Looping through the array of movies
+    // Looping through the array of cities
     for (var i = 0; i < cities.length; i++) {
 
       // Then dynamicaly generating buttons for each city in the array
@@ -18,12 +34,13 @@ var cities = [];
       // Adding a class of movie to our button
       c.addClass("city btn btn-info");
       // Adding a data-attribute
-      c.attr("data-name", cities[i]);
+      c.attr("city-input", cities[i]);
       // Providing the initial button text
       c.text(cities[i]);
       // Adding the button to the HTML
       $("#cityBtn").append(c);
     }
+    
   }
 
   // This function handles events where one button is clicked
@@ -38,8 +55,11 @@ var cities = [];
 
     // Calling renderButtons which handles the processing of our city array
     renderButtons();
-
   });
+// Adding a click event listener to all elements with a class of "movie-btn"
 
-  // Calling the renderButtons function to display the initial buttons
-  renderButtons();
+      // Adding a click event listener to all elements with a class of "movie-btn"
+      $(document).on("click", "#cityBtn", displayWeather());
+
+      // Calling the renderButtons function to display the initial buttons
+      renderButtons();
